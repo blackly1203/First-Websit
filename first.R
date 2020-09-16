5f40ba8b57edecccadd0e7864563e6c209739917
I am I <blackly1203.com>
2020-09-16 20:32:03 +0000

    fine

---
+++ first.R
@@ -0,0 +1,18 @@
+install.packages("tidyverse")
+install.packages("blogdown")
+library("blogdown")
+library("tidyverse")
+?blogdown::new_site()
+
+blogdown::new_site(
+  dir = ".",
+  install_hugo = TRUE,
+  format = "toml",
+  sample = TRUE,
+  theme = "yihui/hugo-lithium",
+  hostname = "github.com",
+  theme_example = TRUE,
+  empty_dirs = FALSE,
+  to_yaml = TRUE,
+  serve = interactive()
+)
